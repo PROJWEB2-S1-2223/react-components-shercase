08548ef81ff8f01f572fdf5d26c32094bcc73ddd
shercase <113887889+shercase@users.noreply.github.com>
2023-01-09 10:35:54 +0000

    liste des courses

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,31 +2,29 @@ import React, { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
 
+const titre=
+  (<h1> "liste des courses"</h1>
+  );
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((countParam) => countParam + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+ <div><Titre /></div>
+<ul>
+
+    <li> pomme <input type= "checkbox"/> </li>
+    <li> poire <input type= "checkbox"/> </li>
+    <li> eau  <input type= "checkbox"/> </li>
+    <li> carotte <input type= "checkbox"/> </li>
+</ul>
+    <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
   );
+  function Titre(){
+    <h1>Liste des courses</h1>
+  }
 }
 
 export default App;
